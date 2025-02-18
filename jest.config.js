1e48bd63750d8a4b77921a78714f09942992ed8e
wfpena <wilsonf.pena@gmail.com>
2025-02-18 12:53:45 -0300

    feat: initial project setup with TypeScript, ESLint, Jest and VS Code config

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/types/**',
+    '!src/index.ts',
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true,
+};
